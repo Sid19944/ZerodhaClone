03b5bd9d8e82dabf52f72e50b530c343972e34ca
Siddharth Sarkar <sidsarkar2112@gmail.com>
2025-12-23 20:20:44 +0530

    fix

--- backend/util/SecretToken.js
+++ backend/util/SecretToken.js
@@ -1,8 +1,8 @@
 require("dotenv").config();
 const jwt = require("jsonwebtoken");
 
-module.exports.CreateAccessToken = (id) => {
-  return jwt.sign({ id }, process.env.TOKEN_KEY, {
+module.exports.CreateAccessToken = (userId) => {
+  return jwt.sign({ id : userId }, process.env.TOKEN_KEY, {
     expiresIn: 3 * 24 * 60 * 60,
   });
 };
