23cea23887eeaf501ea710371fadc6847979866e
Siddharth Sarkar <sidsarkar2112@gmail.com>
2025-12-23 19:27:25 +0530

    fix

--- backend/routes/AuthRoute.js
+++ backend/routes/AuthRoute.js
@@ -2,9 +2,10 @@ const { Singup, Login } = require("../controller/AutoController");
 const {UserVerification} = require("../middleware/AuthMiddleware.js");
 const router = require("express").Router();
 
-router.post("/", UserVerification)
+
 router.post("/singup", Singup);
 router.post("/login", Login);
+router.post("/", UserVerification)
 
 
 module.exports = router;
