ac0cc432b991e5c8d5b22c49794dc0f69d9af451
Siddharth Sarkar <sidsarkar2112@gmail.com>
2025-12-23 18:29:16 +0530

    fix

--- backend/controller/AutoController.js
+++ backend/controller/AutoController.js
@@ -16,7 +16,6 @@ module.exports.Singup = async (req, res) => {
       httpOnly: true,
       secure: true,
       sameSite: "none",
-      domain : "https://zerodhaclone-dashboard-8i8o.onrender.com",
     });
     res
       .status(201)
@@ -46,7 +45,6 @@ module.exports.Login = async (req, res, next) => {
       httpOnly: true,
       secure: true,
       sameSite: "none",
-      domain : "https://zerodhaclone-dashboard-8i8o.onrender.com",
     });
     res
       .status(201)
